bffa1058a6efa10c34fbb66928a0e61e2efdcc6d
Morzharetto Ilya <morzharetto@icloud.com>
2020-03-25 17:13:10 +0300

    Reworked BEAST with improved performance

---
+++ include/TrellisDecoder.h
@@ -0,0 +1,8 @@
+//
+// Created by morzharetto on 3/20/20.
+//
+
+#ifndef BEAST_TRELLISDECODER_H
+#define BEAST_TRELLISDECODER_H
+
+#endif //BEAST_TRELLISDECODER_H
---
+++ src/TrellisDecoder.cpp
@@ -0,0 +1,5 @@
+//
+// Created by morzharetto on 3/20/20.
+//
+
+#include "TrellisDecoder.h"
